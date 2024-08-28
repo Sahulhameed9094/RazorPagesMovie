fb6c23a09da60d905669e3c5b9dab8d3555fad8d
sahulhameed.s <sahulhameed.s@bthinq.com>
2024-08-28 16:33:46 +0530

    Added Crud opertions

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
 
 var app = builder.Build();
+//seeding data DB
+using (var scope= app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -23,3 +39,11 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+
+//app.UseHttpsRedirection(); : Redirects HTTP requests to HTTPS.
+//app.UseStaticFiles(); : Enables static files, such as HTML, CSS, images, and JavaScript to be served. For more information, see Static files in ASP.NET Core.
+//app.UseRouting(); : Adds route matching to the middleware pipeline. For more information, see Routing in ASP.NET Core
+//app.MapRazorPages();: Configures endpoint routing for Razor Pages.
+//app.UseAuthorization(); : Authorizes a user to access secure resources. This app doesn't use authorization, therefore this line could be removed.
+//app.Run(); : Runs the app.
